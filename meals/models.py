70edabd391e014b1be08a855e5bb4a741a541a3e
onadj <oliver.nad@gmail.com>
2020-11-09 11:22:17 +0000

    added slug in meals model and verbose name plural

--- meals/models.py
+++ meals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Meals(models.Model):
@@ -8,6 +9,17 @@ class Meals(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.IntegerField()
     image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+         self.slug = slugify(self.name)
+        super(Meals, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
 
     def __str__(self):
         return self.name
