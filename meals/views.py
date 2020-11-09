4345c0a8acb4cef0bde914de502f9978e4737a19
onadj <oliver.nad@gmail.com>
2020-11-09 12:33:16 +0000

    added templates with detail.html and list.html, updated views.py for meal_list

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Meals
 # Create your views here.
+
+
+def meal_list(request):
+    meal_list = Meals.objects.all()
+    context = {'meal_list' : meal_list,}
+    return render(request, 'Meals/list.html', context)
+
+def meal_detail(request, slug):
+    pass     
