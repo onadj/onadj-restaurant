cf61cc2f75d2d7ecedf13ec838dec72109c02c70
onadj <oliver.nad@gmail.com>
2020-11-09 12:45:16 +0000

    added meals detail view with image

--- meals/views.py
+++ meals/views.py
@@ -9,4 +9,8 @@ def meal_list(request):
     return render(request, 'Meals/list.html', context)
 
 def meal_detail(request, slug):
-    pass     
+    meal_detail = Meals.objects.get(slug=slug)
+    context = {
+        'meal_detail':meal_detail
+    }
+    return render(request, 'Meals/detail.html', context)
