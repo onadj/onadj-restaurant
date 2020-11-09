d84102a4c8616c73b28ead4089c1f61532ccfa8a
onadj <oliver.nad@gmail.com>
2020-11-09 18:01:42 +0000

    added meals by categories

--- meals/models.py
+++ meals/models.py
@@ -5,6 +5,7 @@ from django.utils.text import slugify
 class Meals(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL , null=True)
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.IntegerField()
@@ -20,6 +21,16 @@ class Meals(models.Model):
         verbose_name = 'meal'
         verbose_name_plural = 'meals'
 
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
