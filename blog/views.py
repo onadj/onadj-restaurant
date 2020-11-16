add6e92d83f0c29d37c094b3a7c41215c47e336f
onadj <oliver.nad@gmail.com>
2020-11-16 22:35:22 +0000

    deployment 9

--- blog/views.py
+++ blog/views.py
@@ -24,38 +24,39 @@ def post_list(request):
     page = request.GET.get('page')
     post_list = paginator.get_page(page)
     context = {
-        'post_list': post_list,
+        'post_list' : post_list ,
     }
 
-    return render(request , 'Post/post_list.html', context)
+    return render(request , 'Post/post_list.html' , context)
 
 
 def post_detail(request , id):
     post_detail = Post.objects.get(id=id)
     categories = Category.objects.all()
     all_tags = Tag.objects.all()
-    comments = Comment.objects.filter(post=post_detail.post_id)
+    comments = Comment.objects.filter(post=post_detail)
+    
 
     if request.method == 'POST' :
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.user = request.user
-            new_comment.post = post_detail.post_id
+            new_comment.post = post_detail
             new_comment.save()
 
     else:
         comment_form = CommentForm()
 
     context = {
-        'post_detail' : post_detail.post_id,
-        'categories' : categories,
-        'all_tags' : all_tags,
-        'comments' : comments,
+        'post_detail' : post_detail ,
+        'categories' : categories ,
+        'all_tags' : all_tags ,
+        'comments' : comments ,
         'comment_form' : comment_form
     }
 
-    return render(request , 'Post/post_detail.html', context)
+    return render(request , 'Post/post_detail.html' , context)
 
 
 
@@ -63,10 +64,10 @@ def post_detail(request , id):
 def post_by_tag(request , tag):
     post_by_tag = Post.objects.filter(tags__name__in=[tag])
     context = {
-        'post_list' : post_by_tag,
+        'post_list' : post_by_tag ,
     }
 
-    return render(request , 'Post/post_list.html', context)
+    return render(request , 'Post/post_list.html' , context)
 
 
 
