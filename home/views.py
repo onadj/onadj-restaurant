c0a786d5620a461458f8c4e9ec75c526f08b5e4a
onadj <oliver.nad@gmail.com>
2020-11-20 16:37:28 +0000

    kj

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from meals.models import Meals, Category
+from meals.models import Meals , Category
 from blog.models import Post
 from aboutus.models import Why_Choose_Us
 # Create your views here.
@@ -10,8 +10,6 @@ def home(request):
     meals = Meals.objects.all()
     meal_list = Meals.objects.all()
     categories = Category.objects.all()
-    posts = Post.objects.all()
-    latest_post = Post.objects.last()
     why_choose_us = Why_Choose_Us.objects.all()
 
 
@@ -20,8 +18,6 @@ def home(request):
         'meals' : meals ,
         'meal_list' : meal_list ,
         'categories' : categories ,
-        'posts' : posts ,
-        'latest_post' : latest_post ,
         'why_choose_us' : why_choose_us , 
     }
 
