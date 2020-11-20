0c6d1f667384c95b40d6c04b9f50bdb159de6003
onadj <oliver.nad@gmail.com>
2020-11-20 17:08:07 +0000

    database...

--- home/views.py
+++ home/views.py
@@ -10,15 +10,19 @@ def home(request):
     meals = Meals.objects.all()
     meal_list = Meals.objects.all()
     categories = Category.objects.all()
+    posts = Post.objects.all()
+    latest_post = Post.objects.last()
     why_choose_us = Why_Choose_Us.objects.all()
 
 
     
     context = {
-        'meals' : meals ,
-        'meal_list' : meal_list ,
-        'categories' : categories ,
-        'why_choose_us' : why_choose_us , 
+        'meals': meals,
+        'meal_list': meal_list,
+        'categories': categories,
+        'posts': posts,
+        'latest_post': latest_post,
+        'why_choose_us': why_choose_us, 
     }
 
-    return render(request , 'home/index.html' , context)
+    return render(request,'home/index.html', context)
