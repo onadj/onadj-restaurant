fc126c1681c054c46c64b6efdb96747064b6994e
onadj <oliver.nad@gmail.com>
2020-11-24 14:43:05 +0000

    kjk

--- meals/admin.py
+++ meals/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Meals , Category
 
 
-class MealsAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
+class MealsAdmin(admin.ModelAdmin):  # instead of ModelAdmin
     summernote_fields = '__all__'
     list_display = ['name' ,'preparation_time', 'people', 'price']
     search_fields = ['name', 'description' ]
