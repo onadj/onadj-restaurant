a1d11e03397cad3f2025cef06f6d470fef98e7b6
onadj <oliver.nad@gmail.com>
2020-11-24 13:32:08 +0000

    kj2

--- contact/views.py
+++ contact/views.py
@@ -14,7 +14,7 @@ def send_email(request):
             message = form.cleaned_data['message']
 
             try : 
-                send_mail(subject, message, from_email,['djangorestaurant31@gmail.com'])
+                send_mail(from_email, message, subject, ['djangorestaurant31@gmail.com'])
 
             except BadHeaderError:
                 return HttpResponse('invalid header') 
